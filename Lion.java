d6df9094badca07f07f0f83ef01cbabf8f8c51ab
Faisal-Alsharif <ifaisal.alsharif@gmail.com>
2021-11-16 11:12:53 +0300

    Advance OOP Lab

---
+++ Lion.java
@@ -0,0 +1,30 @@
+
+public class Lion extends Animal {
+	
+	private String type;
+
+	public Lion(int numTeeth, boolean spots, int weight) {
+		super(numTeeth, spots, weight);
+		this.setType(weight);
+	}
+	
+	public String getType() {
+		return this.type;
+	}
+
+	public void setType(int weight) {
+		if(weight < 80){
+			this.type = "cub";
+		}
+		else if(weight < 120){
+			this.type = "female";
+		}
+		else if(weight > 120){
+			this.type = "male";
+		}
+	}
+	
+	public void prinLlionInfo(){
+		System.out.println("Number of Teeth: " + getNumTeeth() + "\nSpots: " + getSpots() + "\nWeight: " + getWeight() + "kg" + "\nType: " + getType() + "\n");
+	}
+}
---
+++ Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+
+
+	public static void main(String[] args) {
+		Lion lion1 = new Lion(14, true, 90);
+		lion1.prinLlionInfo();
+		
+		Lion lion2 = new Lion(16, false, 137);
+		lion2.prinLlionInfo();
+	}
+
+}
