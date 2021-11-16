9b9f1fbb8ae0ecc92bd1154d6c3add3639406f3e
Nouraoqail <Nouraoqail.10@gmail.com>
2021-11-16 12:54:17 +0300

    done

---
+++ Animal/src/com/company/Lion.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Lion extends Animal{
+
+    String lionType;
+
+public Lion(int numTeeth, boolean spots, int weight){
+    super(numTeeth,spots,weight);
+    SetLionType();
+}
+public void SetLionType(){
+    if(getWeight()<80)
+        lionType="cub";
+    else if(getWeight()<120)
+        lionType="female";
+    else
+        lionType="male";
+}
+public void print(){
+    System.out.println(getNumTeeth());
+    System.out.println(getSpots());
+    System.out.println(getWeight());
+}
+}
