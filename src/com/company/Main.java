2a147f5e33cd5b7bd0eea321ecb9f8fcff545466
Nasser-96 <nasserabunasir96@outlook.com>
2021-11-16 11:55:23 +0300

    main added

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 Lion lion = new Lion(30,true,140);
+        lion.printAnimal();
+//        Cheetah cheetah = new Cheetah(30,true,140);
+//        cheetah.printAnimal();
+    }
+}
